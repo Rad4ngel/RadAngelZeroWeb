32725af136f99e4307dbe6492836fac949ccf8ca
RadAngelZero <radangelzero@gmail.com>
2021-10-24 04:11:09 -0500

    Test online

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Eris = require('eris');
+const discordBotEris = new Eris(process.env.REACT_APP_DISCORD_BOT_TOKEN, { restMode: true });
+
+discordBotEris.on('ready', async () => {
+  let self = await discordBotEris.getSelf();
+  // let rad = await discordBotEris.getRESTUser(process.env.REACT_APP_DISCORD_ID)
+  let guilds = discordBotEris.getRESTUser("261700562649612289");
+  console.log('redy ' + self.username + '#' + self.discriminator);
+  console.log(guilds)
+  discordBotEris.editStatus('invisible');
+})
+
+discordBotEris.connect();
+
 function App() {
+  useEffect(() => {
+    console.log(discordBotEris)
+    console.log(process.env)
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
